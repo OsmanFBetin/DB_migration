9ee0f41d932ba115208f22c57459b52e6c86d441
OsmanFBetin <investigadorbiotec@gmail.com>
2023-10-15 18:11:25 -0400

    Adding relationships in the model

--- db/models.py
+++ db/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from db.database import Base
 import datetime
 class DepartmentData(Base):
@@ -19,5 +20,8 @@ class HiredEmployeeData(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=True)
     datetime = Column(DateTime, nullable=True)
-    department_id = Column(Integer, nullable=True)
-    job_id = Column(Integer, nullable=True)
+    department_id = Column(Integer, ForeignKey("departments.id"))
+    job_id = Column(Integer, ForeignKey("jobs.id"))
+
+    depto_r = relationship("departments", back_populates="hired_employees")
+    jobs_r = relationship("jobs", back_populates="hired_employees")
