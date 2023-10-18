2638ee0a6c656b26e25b4ee9946d669d8e6571e1
OsmanFBetin <investigadorbiotec@gmail.com>
2023-10-17 23:40:56 -0400

    Adding test for end-point load-csv

--- test_main.py
+++ test_main.py
@@ -1,3 +1,5 @@
+import io
+import csv
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from main import app 
@@ -8,3 +10,16 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Welcome to the API Migration"}
+
+def test_upload_csv():
+    # Create a mock CSV file as a bytes object
+    csv_data = "186,Developer AWS I\n187,Developer Azure I".encode("utf-8")
+
+    csv_file = io.BytesIO(csv_data)
+
+    # Send a POST request to the endpoint with the mock CSV file
+    response = client.post("/upload-csv/", files={"file": ("jobs.csv", csv_file, "application/json")})
+
+    # Assert the response status code and content
+    assert response.status_code == 200
+    assert response.json() == {"message": "CSV data uploaded successfully"}
